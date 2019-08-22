917eec7a9a99154a935d53e7395fd32b4d31a48a
ajones161 <a.jones235@outlook.com>
2019-08-22 17:37:49 -0300

    added more. small updates to home, fully converted schedule pages to a dark theme.

---
+++ js/konami.js
@@ -0,0 +1,52 @@
+function onKonamiCode(cb) {
+    var input = '';
+    var key = '38384040373937396665';
+    document.addEventListener('keydown', function (e) {
+      input += ("" + e.keyCode);
+      if (input === key) {
+        return cb();
+      }
+      if (!key.indexOf(input)) return;
+      input = ("" + e.keyCode);
+    });
+  }
+  
+  onKonamiCode(function () {
+      let bing = document.getElementById("konami");
+      if(bing.classList.contains("hide")) {
+          unfade(bing);
+          bing.classList.remove("hide");
+      }
+      else {
+          fade(bing);
+          bing.classList.add("hide");
+      }
+  })
+
+  function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+function fade(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+  
