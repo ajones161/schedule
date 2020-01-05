7bf178d0c1284e6438132000ca6caf7f5680631f
ajones161 <a.jones235@outlook.com>
2020-01-05 18:27:44 -0400

    Update for Winter Term.

--- js/schedule.js
+++ js/schedule.js
@@ -2,6 +2,8 @@ window.onload = function () {
     /* This small switch looks for a tag embedded in the header title, which tells it which class json to look for.
     Once the switch picks the right json, it dumps it into an intentionally vague variable to be loaded into an xml request.
     I could have just copied the script into a .js file for every page, but this means I have less documents to look after. */
+    checkTimer();
+
     let file;
     let getTitle = document.getElementById('headTitle');
     let className = getTitle.className;
@@ -12,15 +14,12 @@ window.onload = function () {
         case "na2":
             file = "/jsons/na2.json";
             break;
-            //case "c1":
-            //    file = "/jsons/c1.json";
-            //    break;
-            //case "pa1":
-            //    file="/jsons/pa1.json";
-            //    break;
-            //case "na1":
-            //    file="/jsons/na1.json";
-            //    break;
+        case "pa1":
+            file = "/jsons/pa1.json";
+            break;
+        case "na1":
+            file = "/jsons/na1.json";
+            break;
         default:
             break;
     }
@@ -33,7 +32,24 @@ window.onload = function () {
     };
     xml.open("GET", file, true);
     xml.send();
+
+    console.log(document.cookie);
+    if (document.cookie == "bingBong") {
+        let bing = document.getElementById("konami");
+        bing.classList.remove("hide");
+        bing.style.opacity = 1;
+    }
+
+
 }
+
+/* Reloads page on regaining focus. 
+Literally just so I can leave the page open without having to refresh manually every time I look at it again*/
+window.onblur = function () {
+    window.onfocus = function () {
+        location.reload(true)
+    }
+};
 
 /* This function builds the schedule grid. First it parses the json it's passed. */
 function buildGrid(text) {
@@ -81,17 +97,11 @@ function buildGrid(text) {
                 case "na2":
                     colour = "na2";
                     break;
-                case "it11":
-                    colour = "it11";
+                case "pa1":
+                    colour = "pa1";
                     break;
-                case "it12":
-                    colour = "it12";
-                    break;
-                case "it13":
-                    colour = "na2";
-                    break;
-                default:
-                    colour = "it1Shared";
+                case "na1":
+                    colour = "na1";
                     break;
             }
             /* This applies a class I have set aside to determine the grid width of the item currently being dropped in. */
@@ -104,13 +114,12 @@ function buildGrid(text) {
                 fillString = '<div id="' +
                     checking.classStart + '" class="' + dow + ' ' + colour + ' ' + width + '">' +
                     checking.className + '<br>' + checking.teacherName + checking.roomNumber + '</div>';
-            } 
-            else if (checking.type == "split") {
+            } else if (checking.type == "split") {
                 fillString = '<div id="' +
                     checking.classStart + '" class="' + dow + ' ' + colour + ' ' + width + '">' +
-                    '<div class="split">' + innerString + '</div>' +'<div class="split">' + innerString2 + '</div>' +
-                     '</div>';
-            } 
+                    '<div class="split">' + innerString + '</div>' + '<div class="split">' + innerString2 + '</div>' +
+                    '</div>';
+            }
 
 
             grid.innerHTML += fillString;
@@ -207,7 +216,18 @@ function checkShort(time) {
     } else {
         fixed = time;
     }
+
+    if (fixed >= 2400) {
+        fixed = tooLarge(fixed);
+    }
     return fixed;
+}
+
+function tooLarge(time) {
+    let split = time.split("");
+    let splice = split.splice(2, 1);
+    time = split.join("");
+    return time;
 }
 
 function fillMobile(day, nodes) {
@@ -269,4 +289,29 @@ function formatTimes(time) {
     fixed = split.join("");
 
     return fixed;
+}
+
+function checkTimer() {
+    let countDownDate, x, now, distance, days, hours, minutes, seconds;
+    countDownDate = new Date("Dec 20, 2019 16:20:00").getTime();
+
+    x = setInterval(function () {
+
+        now = new Date().getTime();
+
+        distance = countDownDate - now;
+
+        days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("timer").innerHTML = days + "d " + hours + ":" +
+            minutes + ":" + seconds + " until end of term.";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    }, 1000);
 }
